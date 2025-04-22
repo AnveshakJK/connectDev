de3615e5493fd0958041f9c9af45e86f15243b54
Your Name <you@example.com>
2025-04-22 22:23:29 +0530

    route handler

--- app.js
+++ app.js
@@ -1,25 +1,57 @@
 const express = require("express");
 const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send({firstname:"hello",lastname:"JK"});
-});
+/*
+app.use("/user",(req,res,next)=>{
+    // route handler 
+    console.log("route handler from 1");
+    
+    res.send("route handler 1"); 
+    next(); 
+    // //if not in res.send then not further as line by line code it excute then call stack get empty not in there . because express not known what to do next but this in node not problem . so use next(); and also pass in argument also .
+    
+    // error get by this
+    // next();
+    // res.send("route handler 1"); 
+}, 
+  (req,res)=>{
+    console.log("route handler from 2");
+    res.send("response 2");
+    // next();
+},
+  (req,res)=>{
+    console.log("route handler from 3");
+    res.send("response 3");
+    // next();
+},
+  (req,res)=>{console.log("route handler from 4");
+    res.send("response 4");
+    next();  // at last in do then get error due to next not route handler to call this , call stack get empty.
+}
+); 
+*/
 
-app.post("/user",(req,res)=>{
-    res.send("data saved");
-});
-
-app.put("/user",(req,res)=>{
-    res.send("this put happen");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Deletion of data done");
-});
-
-app.use("/user",(req,res)=>{
-    res.send("method of used use for all http method.");
-});
+app.use("/user",(req,res,next)=>{
+    // route handler 
+    console.log("route handler from 1");
+    
+    // res.send("route handler 1"); 
+    next(); 
+}, [
+  (req,res,next)=>{
+    console.log("route handler from 2");
+    res.send("response 2");
+    next();
+},
+  (req,res)=>{
+    console.log("route handler from 3");
+    res.send("response 3");
+    // next();
+}],
+  (req,res)=>{console.log("route handler from 4");
+    res.send("response 4");
+}
+); 
 
 app.listen(3000,()=>{
     console.log("Listen to port 3000");
